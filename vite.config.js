2ba84c555796286a0a400f02b5236ca07c1fac8a
Grappler2010 <yakhyaev2008@mail.ru>
2024-03-12 16:21:49 +0300

    Add github-base-url

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif']
+  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
+  base: 'https://aitrade2024.github.io/ai-trade/'
 })

a51c695a249381db2073e6f1de1f4ce8e237a0b7
Grappler2010 <yakhyaev2008@mail.ru>
2024-03-13 23:58:28 +0300

    Add the animation for step-section

--- src/script.js
+++ src/script.js
@@ -71,3 +71,30 @@ setInterval(renderTable, 2000);
   await initEngine(containerElement)
   startApp()
 })();
+
+const scrollOffset = 100;
+const scrollElement = document.querySelector(".scroll");
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+const displayScrollElement = () => {
+  scrollElement.classList.add('scrolled');
+}
+const hideScrollElement = () => {
+  scrollElement.classList.remove('scrolled');
+}
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
